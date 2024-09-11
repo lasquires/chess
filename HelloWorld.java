be8e73ebd4875d1fedeed49c02c000adf892beb3
lasquires <lukesquires@gmail.com>
2024-09-11 14:36:08 -0600

    git commit -am "init(phase0)"

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,11 +1,14 @@
-
-
 public class HelloWorld {
     public static void main(String[] args){
         System.out.println("Hello World!");
 
         var p = new Person("James");
         p.sleep();
+        System.out.println("\n");
+        String[] names = {"fee", "fi", "fo", "fum"};
+        for (var i =0; i < names.length; i++){
+            System.out.println(names[i]);
+        }
     }
 }
 
