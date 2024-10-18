9815e0f0bc597d96f59fb1d2182b72ec50a52ff7
lasquires <lukesquires@gmail.com>
2024-10-18 13:50:08 -0600

    implemented preliminary MemoryAuthDAO

--- server/src/main/java/dataaccess/AuthDAO.java
+++ server/src/main/java/dataaccess/AuthDAO.java
@@ -3,7 +3,7 @@ package dataaccess;
 import model.AuthData;
 
 public interface AuthDAO {
-    void createAuth(AuthData auth) throws DataAccessException;
+    void createAuth(AuthData authData) throws DataAccessException;
     AuthData getAuth(String authToken) throws DataAccessException;
     void deleteAuth(String authToken) throws DataAccessException;
     void clear() throws DataAccessException;
--- server/src/main/java/dataaccess/MemoryAuthDAO.java
+++ server/src/main/java/dataaccess/MemoryAuthDAO.java
@@ -1,31 +1,30 @@
 package dataaccess;
 
 import model.AuthData;
-import model.UserData;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class MemoryAuthDAO implements AuthDAO {
-    private Map<String, AuthData> auth = new HashMap<>();
+    private Map<String, AuthData> authTokens = new HashMap<>();     //{authToken: AuthData, ...}
 
     @Override
-    public void createAuth(AuthData auth) throws DataAccessException {
-
+    public void createAuth(AuthData authData) throws DataAccessException {
+        authTokens.put(authData.authToken(), authData);     //authTokens[authToken]=authData
     }
 
     @Override
     public AuthData getAuth(String authToken) throws DataAccessException {
-        return null;
+        return authTokens.get(authToken);
     }
 
     @Override
     public void deleteAuth(String authToken) throws DataAccessException {
-
+        authTokens.remove(authToken);
     }
 
     @Override
     public void clear() throws DataAccessException {
-
+        authTokens.clear();
     }
 }
