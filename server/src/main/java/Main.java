e78af12c22be77bab7067e82fe29e630575d9f13
lasquires <lukesquires@gmail.com>
2024-10-18 13:05:53 -0600

    Added the Data Model Classes

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -1,12 +1,18 @@
 import chess.*;
+import server.Server;
+
 
 public class Main {
     public static void main(String[] args) {
+        Server server = new Server();
+
+        int port = server.run(8080);
+
+        System.out.println("Server is running on port: " + port);
 
 
 
-
-        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
-        System.out.println("♕ 240 Chess Server: " + piece);
+//        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
+//        System.out.println("♕ 240 Chess Server: " + piece);
     }
 }
