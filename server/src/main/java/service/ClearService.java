ca24a5f66d3f4043861b5f06101adfd16b8eff36
lasquires <lukesquires@gmail.com>
2024-10-18 17:10:07 -0600

    Implemented ClearService.java

---
+++ server/src/main/java/service/ClearService.java
@@ -0,0 +1,14 @@
+package service;
+
+import dataaccess.*;
+
+public class ClearService {
+    private GameDAO gameDAO;
+    private AuthDAO authDAO;
+    private UserDAO userDAO;
+    public void clear() throws DataAccessException{
+        DataAccess.getAuthDAO().clear();
+        DataAccess.getGameDAO().clear();
+        DataAccess.getUserDAO().clear();
+    }
+}
