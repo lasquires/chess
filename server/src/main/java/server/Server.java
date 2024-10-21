6936c9c4230955c86494223763af02eaab7e6692
lasquires <lukesquires@gmail.com>
2024-10-21 15:46:13 -0600

    startercode?

---
+++ server/src/main/java/server/Server.java
@@ -0,0 +1,33 @@
+package server;
+
+import handler.*;
+import spark.*;
+
+public class Server {
+
+    public int run(int desiredPort) {
+        Spark.port(desiredPort);
+
+        Spark.staticFiles.location("web");
+
+        // Register your endpoints and handle exceptions here.
+        Spark.post("/user", new RegisterHandler());
+        Spark.post("/session", new LoginHandler());
+        Spark.delete("/session", new LogoutHandler());
+        Spark.get("/game", new ListGameHandler());
+        Spark.post("/game", new CreateGameHandler());
+        Spark.put("/game", new JoinGameHandler());
+        Spark.delete("/db", new ClearHandler());
+
+//        //This line initializes the server and can be removed once you have a functioning endpoint
+//        Spark.init();
+
+        Spark.awaitInitialization();
+        return Spark.port();
+    }
+
+    public void stop() {
+        Spark.stop();
+        Spark.awaitStop();
+    }
+}
