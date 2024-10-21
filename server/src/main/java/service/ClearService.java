5745b8d84620680e92b023bfe8cab2ac253c896d
lasquires <lukesquires@gmail.com>
2024-10-21 16:21:21 -0600

    removed commented out code

--- server/src/main/java/service/ClearService.java
+++ server/src/main/java/service/ClearService.java
@@ -3,9 +3,6 @@ package service;
 import dataaccess.*;
 
 public class ClearService {
-//    private GameDAO gameDAO;
-//    private AuthDAO authDAO;
-//    private UserDAO userDAO;
     public void clear() throws DataAccessException{
         DataAccess.getAuthDAO().clear();
         DataAccess.getGameDAO().clear();
