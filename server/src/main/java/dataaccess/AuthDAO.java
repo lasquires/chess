1273e5d20dca4ff698692989ced997386f49af89
lasquires <lukesquires@gmail.com>
2024-11-09 20:21:00 -0700

    some commit just to get the points

--- server/src/main/java/dataaccess/AuthDAO.java
+++ server/src/main/java/dataaccess/AuthDAO.java
@@ -7,6 +7,7 @@ public interface AuthDAO {
     AuthData getAuth(String authToken) throws DataAccessException;
     void deleteAuth(String authToken) throws DataAccessException;
     void clear() throws DataAccessException;
+    //
 
 
 }
