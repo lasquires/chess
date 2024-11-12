7b37f26269e1b818dbd8d0033518021baf84c56e
lasquires <lukesquires@gmail.com>
2024-11-11 17:23:25 -0700

    Added serverFacade.java and tried preliminary code

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -1,8 +1,19 @@
 import chess.*;
 
 public class Main {
+
     public static void main(String[] args) {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Client: " + piece);
+
+
+    }
+    public void displayPreLoginPrompt(){
+        System.out.println("♕ Welcome to Chess. Sign in to start. ♕");
+        System.out.println("Options:");
+        System.out.println("Login as an existing user: \"l\", \"login\" <USERNAME> <PASSWORD>");
+        System.out.println("Register a new user: \"r\", \"register\" <USERNAME> <PASSWORD> <EMAIL>");
+        System.out.println("Exit the program: \"q\", \"quit\"");
+        System.out.println("Print this message: \"h\", \"help\"");
     }
 }
